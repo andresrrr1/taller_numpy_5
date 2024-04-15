c6e5907504e5035403c135c3fd6fe280205d7c83
andresrrr1 <andresromero5455@gmail.com>
2024-04-15 15:15:43 -0500

    v 1.0.0

--- programa.py
+++ programa.py
@@ -1,3 +1,17 @@
-#taller numpy
+#taller: calculos numericos en arreglos n dimencionales 
+#narray de 2 dimeciones a partir de la lista 
 import numpy as np
+import matplotlib as plt
+
+a=np.array([[1,5],[7,9]])
+b=np.array([[2,0],[1,3]])
+
+
+#producto 
+c=np.dot(a,b)
+#solucion de un sistema de ecuaciones con numpy 
+solucion=np.array([5,17])
+
+m=np.linalg.solve(a,solucion)
+print(m)
 
